c33d6207cb4b22984a8b665fa57f379cf5f97ffd
babekz <niko.halikka@gmail.com>
2016-06-22 15:20:42 +0300

    add forward_declare.h

---
+++ src/forward_declare.h
@@ -0,0 +1,10 @@
+#pragma once
+
+#define FORWARD_DECLARE_TYPE_STRUCT			struct
+#define FORWARD_DECLARE_TYPE_CLASS			class
+#define FORWARD_DECLARE_TYPE_ENUM_CLASS		enum class
+
+#define FORWARD_DECLARE(__fdtype__, __name__)								__fdtype__ __name__;
+#define FORWARD_DECLARE_1(__fdtype__, __name__, __ns1__)					namespace __ns1__ { __fdtype__ __name__; }
+#define FORWARD_DECLARE_2(__fdtype__, __name__, __ns1__, __ns2__)			namespace __ns1__ { namespace __ns2__ { __fdtype__ __name__; } } 
+#define FORWARD_DECLARE_3(__fdtype__, __name__, __ns1__, __ns2__, __ns3__)	namespace __ns1__ { namespace __ns2__ { namespace __ns3__ { __fdtype__ __name__; } } }
