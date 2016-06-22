004462099f7519768e1943ad3bd675772f10b675
Jani Niemelä <squual@kahvipaussi.net>
2016-06-22 10:56:16 +0300

    fix shader compiling

---
+++ embed_shaders.bat
@@ -0,0 +1,15 @@
+::@ECHO OFF
+cd assets\shaders\
+
+
+..\..\.build\win64_vs2015\bin\shadercRelease.exe -i . -f basic\vs_basic.sc -o dx11\vs_basic.bin --type vertex --platform windows -p vs_5_0 -O 3 
+..\..\.build\win64_vs2015\bin\shadercRelease.exe -i . -f basic\fs_basic.sc -o dx11\fs_basic.bin --type fragment --platform windows -p ps_5_0 -O 3
+
+
+REM embeds
+
+..\..\.build\win64_vs2015\bin\shadercRelease.exe -i . -f basic\vs_basic.sc -o ..\..\src\graphics\vs_texture.bin.h --type vertex --platform windows -p vs_5_0 -O 3 --bin2c vs_texture_dx11
+..\..\.build\win64_vs2015\bin\shadercRelease.exe -i . -f basic\fs_basic.sc -o ..\..\src\graphics\fs_texture.bin.h --type fragment --platform windows -p ps_5_0 -O 3 --bin2c fs_texture_dx11
+
+
+cd ..\..
--- shaders.bat
+++ shaders.bat
@@ -6,5 +6,10 @@ REM msbuild .build\projects\vs2015\shaderc.vcxproj /target:Build /property:Confi
 
 
 cd assets\shaders\
-compile.bat
-cd ..\..\
+
+
+..\..\.build\win64_vs2015\bin\shadercRelease.exe -i . -f basic\vs_basic.sc -o dx11\vs_basic.bin --type vertex --platform windows -p vs_5_0 -O 3 
+..\..\.build\win64_vs2015\bin\shadercRelease.exe -i . -f basic\fs_basic.sc -o dx11\fs_basic.bin --type fragment --platform windows -p ps_5_0 -O 3
+
+
+cd ..\..
