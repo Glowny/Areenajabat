17120571752b78cf6f8027b1b3da3de4e6c32ac4
babekz <niko.halikka@gmail.com>
2016-06-22 15:07:27 +0300

    added types.h

---
+++ src/arena_types.h
@@ -0,0 +1,44 @@
+#pragma once
+
+#include "inttypes.h"
+#include <string>
+
+/*
+	Pointer types.
+*/
+
+using IntPtr	= intptr_t;
+using VoidPtr	= void*;
+
+/*
+	Char/string types.
+*/
+
+using Char		= char;
+using CharArray	= char*;
+using String	= std::string;
+
+/*
+	Signed types.
+*/
+
+using int8		= int8_t;
+using int16		= int16_t;
+using int32		= int32_t;
+using int64		= int64_t;
+
+/*
+	Unsigned types.
+*/
+
+using uint8		= uint8_t;
+using uint16	= uint16_t;
+using uint32	= uint32_t;
+using uint64	= uint64_t;
+
+/*
+	Floating point types.
+*/
+
+using float32	= float;
+using float64	= double;
