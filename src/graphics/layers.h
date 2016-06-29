e725cc74c1555ccc83acde781ce82f3bd19b54ca
babekz <niko.halikka@gmail.com>
2016-06-29 15:37:55 +0300

    added layers

---
+++ src/graphics/layers.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include "..\arena_types.h"
+
+namespace arena
+{
+	namespace layers
+	{
+		const uint32 Background = 0;	
+		const uint32 Middle		= 1;	
+		const uint32 Foreground = 2;	
+		const uint32 Effects	= 3;
+		const uint32 HUD		= 4;
+	}
+}
