c3f1df3e29ae93112dc56a4ae96bdf2efa870dca
babekz <niko.halikka@gmail.com>
2016-07-08 12:49:56 +0300

    fix syntax error

--- common/forward_declare.h
+++ common/forward_declare.h
@@ -7,4 +7,4 @@
 #define FORWARD_DECLARE(__fdtype__, __name__)								__fdtype__ __name__;
 #define FORWARD_DECLARE_1(__fdtype__, __ns1__, __name__)					namespace __ns1__ { __fdtype__ __name__; }
 #define FORWARD_DECLARE_2(__fdtype__, __ns1__, __ns2__, __name__)			namespace __ns1__ { namespace __ns2__ { __fdtype__ __name__; } } 
-#define FORWARD_DECLARE_3(__fdtype__, __ns1__, __ns2__, __ns3__, __name__)	namespace __ns1__ { namespace __ns2__ { namespace __ns3__ { __fdtype__ __name__; } } }#pragma once
+#define FORWARD_DECLARE_3(__fdtype__, __ns1__, __ns2__, __ns3__, __name__)	namespace __ns1__ { namespace __ns2__ { namespace __ns3__ { __fdtype__ __name__; } } }
