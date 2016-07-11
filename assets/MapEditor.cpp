570ab35cbd477695ff16577f9a849877de2a257e
Vesakar <Vesakar@AOTA1-1L155-19D>
2016-07-11 08:19:31 +0300

    Added latest map editor to assets.

--- assets/MapEditor.cpp
+++ assets/MapEditor.cpp
@@ -4,26 +4,49 @@
 #include <string>
 #include <iostream>
 #include <fstream>
-sf::Color colors[2]{ sf::Color::Blue, sf::Color::Red};
-static unsigned color;
+#include <stdlib.h>
+#include <stdint.h>
+sf::Color colors[4]{ sf::Color::Blue, sf::Color::Red, sf::Color::Green, sf::Color::Magenta};
+
 sf::Text text;
 sf::Text text2;
 sf::Font font;
 float zoom;
 
 sf::Texture texture;
+
+enum PlatformType
+{
+	CollideAll = uint32_t(0),
+	CollideGladiator =uint32_t(1),
+	Ladder = uint32_t(2)
+};
+
+enum PlatformMode
+{
+	Shapeless,
+	Polygon
+};
+
 struct Platform
 {
-	
+	PlatformType type;
 	std::vector<sf::Vector2i> points;
 	std::vector<sf::Vertex> drawPoints;
 	void addPoint(sf::Vector2i point)
 	{
 		sf::Vertex vertex = sf::Vertex(sf::Vector2f(point));
-		vertex.color = colors[color];
+		vertex.color = colors[3];
 		drawPoints.push_back(vertex);
 		points.push_back(point);
 	};
+	void reColor(PlatformType color)
+	{
+		for (unsigned i = 0; i < drawPoints.size(); i++)
+		{
+			drawPoints[i].color = colors[color];
+		}
+	}
 	void clear()
 	{
 		points.clear();
@@ -79,35 +102,87 @@ void save()
 	myfile.close();
 }
 
+
+struct platformObject
+{
+
+	uint32_t size;
+	std::vector<float> pointsX;
+	std::vector<float> pointsY;
+	PlatformType type;
+};
+
 void saveRaw()
 {
 
 	std::ofstream myfile;
-	myfile.open("coordinatesRaw.txt");
-	std::string text;
+	myfile.open("coordinatesRawData.dat",  std::ios::out| std::ios::binary );
+	uint32_t size = platforms.size();
+	
+
+	myfile.write(reinterpret_cast<char *>(&size), sizeof(uint32_t));
 	for (int i = 0; i < platforms.size(); i++)
 	{
-		text += std::to_string(i);
-		text += "\n";
+		platformObject object;
+		unsigned type = platforms[i].type;
+
+		uint32_t pointSize = platforms[i].points.size();
 		for (unsigned j = 0; j < platforms[i].points.size(); j++)
 		{
-			text += std::to_string(platforms[i].points[j].x);
-			text += ", ";
-			text += std::to_string(platforms[i].points[j].y);
-			text += "\n";
-		}
+			object.pointsX.push_back(platforms[i].points[j].x);
+			object.pointsY.push_back(platforms[i].points[j].y);
+		}aaw
+		myfile.write(reinterpret_cast<char *>(&pointSize), sizeof(uint32_t));
+		myfile.write(reinterpret_cast<char *>(&type), sizeof(uint32_t));
+		myfile.write(reinterpret_cast<char *>(object.pointsX.data()), sizeof(float)*object.pointsX.size());
+		myfile.write(reinterpret_cast<char *>(object.pointsY.data()), sizeof(float)*object.pointsY.size());
 	}
-	myfile << text;
-
+	
 	myfile.close();
+
+	std::ifstream file;
+
+	file.open("coordinatesRawData.dat", std::ios::in | std::ios::binary);
+	
+	uint32_t sizes;
+	file.read(reinterpret_cast<char*>(&sizes), sizeof(uint32_t));
+	printf("size: %d\n", sizes);
+	std::vector<platformObject> objects;
+	for (unsigned i = 0; i < sizes; i++)
+	{
+		platformObject object;
+		file.read(reinterpret_cast<char*>(&object.size), sizeof(uint32_t));
+		file.read(reinterpret_cast<char*>(&object.type), sizeof(uint32_t));
+		printf("object type: %d object size: %d\n", object.type,object.size);
+
+		for (unsigned i = 0; i < object.size; i++)
+		{
+			float x;
+			file.read(reinterpret_cast<char*>(&x), sizeof(float));
+			object.pointsX.push_back(x);
+		}
+		for (unsigned i = 0; i < object.size; i++)
+		{
+			float y;
+			file.read(reinterpret_cast<char*>(&y), sizeof(float));
+			object.pointsX.push_back(y);
+		}
+		objects.push_back(object);
+	}
+	file.close();
+
+
 }
+
 int main()
 {
+	PlatformType currentType = CollideAll;
+	PlatformMode mode = Shapeless;
 	zoom = 1;
 	texture.loadFromFile("Map.png");
 	font.loadFromFile("asd.ttf");
 	sf::Sprite sprite(texture);
-	color = 0;
+
 	sf::RenderWindow window(sf::VideoMode(1500, 1000), "SFML works!");
 
 	text.setFont(font);
@@ -115,7 +190,7 @@ int main()
 	text.setCharacterSize(35);
 	text.setColor(sf::Color::White);
 	sf::Vertex vertex;
-	vertex.color = colors[color];
+	vertex.color = colors[3];
 	vertex.position = sf::Vector2f(window.mapPixelToCoords(sf::Mouse::getPosition(window)));
 
 	
@@ -129,11 +204,12 @@ int main()
 	{
 		std::string coordinates;
 		sf::Vector2i intcoords = sf::Vector2i(window.mapPixelToCoords(sf::Mouse::getPosition(window)));
+
 		coordinates += std::to_string(intcoords.x);
 		coordinates += ", ";
 		coordinates += std::to_string(intcoords.y);
 		text.setString(coordinates);
-		vertex.color = colors[color];
+		vertex.color = colors[3];
 		vertex.position = sf::Vector2f(intcoords);
 		platform.drawPoints[platform.drawPoints.size() - 1] = vertex;
 		sf::Event event;
@@ -166,12 +242,17 @@ int main()
 				if (platform.points.size() != 0)
 				{
 					platform.drawPoints.pop_back();
+					if (mode == Polygon)
+					{ 
+						platform.addPoint(platform.points[0]);
+					}
+					platform.type = currentType;
+					printf("%d\n", platform.type);
+					platform.reColor(platform.type);
 					platforms.push_back(platform);
 					platform.clear();
 					platform.drawPoints.push_back(vertex);
-					color++;
-					if (color == 2)
-						color = 0;
+
 				}
 			}
 		}
@@ -181,35 +262,66 @@ int main()
 	
 		if (sf::Keyboard::isKeyPressed(sf::Keyboard::A))
 		{
-			view1.move(-1, 0);
+			view1.move(-2, 0);
 		}
 		if (sf::Keyboard::isKeyPressed(sf::Keyboard::D))
 		{
-			view1.move(1, 0);
+			view1.move(2, 0);
 		}
 		if (sf::Keyboard::isKeyPressed(sf::Keyboard::W))
 		{
-			view1.move(0, -1);
+			view1.move(0, -2);
 		}
 		if (sf::Keyboard::isKeyPressed(sf::Keyboard::S))
 		{
-			view1.move(0, 1);
+			view1.move(0, 2);
 		}
 		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Q))
 		{
 
-			view1.zoom(1.001);
+			view1.zoom(1.005);
 		}
 		if (sf::Keyboard::isKeyPressed(sf::Keyboard::E))
 		{
 
-			view1.zoom(0.999);
+			view1.zoom(0.995);
 		}
 
 		if (sf::Keyboard::isKeyPressed(sf::Keyboard::P))
 		{
 			save();
 			saveRaw();
+		}
+
+		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Num1))
+		{
+			currentType = CollideAll;
+			printf("Platform collides all\n");
+		}
+
+		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Num2))
+		{
+			currentType = CollideGladiator;
+			printf("Platform collides gladiator\n");
+		}
+
+		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Num3))
+		{
+			currentType = Ladder;
+			printf("Platform is ladder\n");
+			mode = Polygon;
+			printf("Platform is polygon\n");
+		}
+
+		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Num4))
+		{
+			mode = Shapeless;
+			printf("Platform is shapeless\n");
+		}
+		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Num5))
+		{
+			mode = Polygon;
+			printf("Platform is polygon\n");
 		}
 
 		window.clear();
